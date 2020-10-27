18364ad9656b0371cc16794f882155d1a532e80f
Abdelra6 <43535792+Abdelra6@users.noreply.github.com>
2020-10-27 19:53:52 +0600

    Update sieve.java

--- sieve.java
+++ sieve.java
@@ -1,29 +1,18 @@
-class SieveOfEratosthenes 
-{ 
-    void sieveOfEratosthenes(int n) 
+
+ static boolean prime[] = new boolean[(int)1e7];     
+static void sieveOfEratosthenes() 
     { 
-        // Create a boolean array "prime[0..n]" and initialize 
-        // all entries it as true. A value in prime[i] will 
-        // finally be false if i is Not a prime, else true. 
-        boolean prime[] = new boolean[n+1]; 
-        for(int i=0;i<n;i++) 
+       
+        for(int i=0;i<(int)1e7;i++) 
             prime[i] = true; 
           
         for(int p = 2; p*p <=n; p++) 
         { 
-            // If prime[p] is not changed, then it is a prime 
-            if(prime[p] == true) 
+           if(prime[p] == true) 
             { 
-                // Update all multiples of p 
-                for(int i = p*2; i <= n; i += p) 
+               for(int i = p*2; i <= n; i += p) 
                     prime[i] = false; 
             } 
         } 
-          
-        // Print all prime numbers 
-        for(int i = 2; i <= n; i++) 
-        { 
-            if(prime[i] == true) 
-                System.out.print(i + " "); 
-        } 
-    } 
+    }
+       
